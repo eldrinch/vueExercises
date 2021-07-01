0625ce9a01b6dd8a865e2a1b0c110c2b766b116a
eldrinch <eldrinc@hotmail.com>
2021-07-01 10:43:42 -0300

    router overview

---
+++ vue/router-overview/src/components/User.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>User {{ $route.params.userId }}</h1>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route);
+  },
+};
+</script>
+<style></style>
