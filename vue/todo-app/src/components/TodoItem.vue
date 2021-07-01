0251be4d64826b708e9a99a6e2efabe45fccb698
eldrinch <eldrinc@hotmail.com>
2021-07-01 04:16:06 -0300

    desafio CRUD e localStorage usage

---
+++ vue/todo-app/src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <InputText v-model="description" />
+    <div calss="div-botoes">
+      <Button @click="cancel" class="btn-voltar">Voltar</Button>
+      <Button @click="saveTodo">Salvar</Button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      description: "",
+    };
+  },
+  props: {
+    todo: Object,
+  },
+  created() {
+    if (this.todo) {
+      this.description = this.todo.description;
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+    saveTodo() {
+      this.$emit("saveTodo", { ...this.todo, description: this.description });
+    },
+  },
+};
+</script>
+<style>
+.div-botoes {
+  margin-top: 1rem;
+}
+
+.btn-voltar {
+  margin-right: 1rem;
+}
+</style>
--- vue/todo-app/src/main.js
+++ vue/todo-app/src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
+//Componenetes Globais
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.use(PrimeVue);
 
 app.mount("#app");
